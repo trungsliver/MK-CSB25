00e51d7e95cc703312f174b09bf1d46ae20c50da
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-19 09:03:47 +0700

    Lesson 1

--- Lesson1/basic.py
+++ Lesson1/basic.py
@@ -45,3 +45,35 @@ Tuổi: {age}
 Điểm: {score}
 Giới tính nam: {is_male}
 ===============================''')
+
+    # Lưu ý:
+        # \n: xuống dòng
+print('Dòng 1\nDòng 2\nDòng 3')
+        # \t: tab (cách = 4 dấu cách)
+print('Cột 1\tCột 2\tCột 3')
+
+
+# Kiểm tra kiểu dữ liệu - type()
+print("Kiểu dữ liệu của biến name:", type(name))
+print("Kiểu dữ liệu của biến age:", type(age))
+print("Kiểu dữ liệu của biến score:", type(score))
+print("Kiểu dữ liệu của biến is_male:", type(is_male))
+
+# Nhập dữ liệu từ bàn phím - input()
+    # Mặc định: dữ liệu nhập vào là string
+score1 = input('Nhập điểm: ')
+print('Kiểu dữ liệu của score1:', type(score1))
+    # Xác đinh kiểu dữ liệu khi nhập vào
+score2 = float(input('Nhập điểm: '))
+print('Kiểu dữ liệu của score2:', type(score2))
+
+# Các phép toán
+    # Cơ bản: + - * /
+    # Chia lấy nguyên: //
+    # Chia lấy dư: %
+    # Lũy thừa: **
+# Lưu ý: lũy thừa sẽ thực hiện từ phải qua trái
+print('7 / 2 =', 7 / 2)
+print('7 // 2 =', 7 // 2)
+print('7 % 2 =', 7 % 2)
+print('2 ** 2 ** 3 =', 2 ** 2 ** 3)  # 2 ** (2 ** 3) = 2 ** 8 = 256
