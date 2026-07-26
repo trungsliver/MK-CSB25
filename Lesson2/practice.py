0a2f453e25b9f4f2fb8949dff1acde6e0d71ab9a
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-26 09:49:33 +0700

    Lesson 2

---
+++ Lesson2/practice.py
@@ -0,0 +1,26 @@
+# Bài 1: Nhập 2 số nguyên a và b từ bàn phím.
+# In ra các số nguyên trong khoảng [a, b]
+
+# Bài 2: Nhập 1 số nguyên a trong khoảng [1, 10]
+# In ra màn hình bảng cửu chương a
+
+# Bài 3: In ra màn hình bảng cửu chương từ 2 => 9
+
+# Bài 4: Nhập số nguyên n trong khoảng [0,100]
+# nếu nhập sai (n<0 hoặc n>100) thì yêu cầu nhập lại
+
+# Bài 5: Cho danh sách num_list
+num_list = [5, 2, 9, 7, 1, 6, 3, 8, 4]
+    # Tính tổng các phần tử chẵn trong danh sách
+    # Đếm số lượng phần tử lẻ trong danh sách
+    # Tìm vị trí phần tử lớn nhất trong danh sách
+
+# Bài 6: Nhập 1 số nguyên n, Kiểm tra xem n có phải số nguyên tố không?
+
+# Bài 7: Viết một hàm sum_odd(numbers) để tính tổng các số lẻ trong một danh sách numbers.
+# 	YC1: Hàm nhận vào một danh sách các số nguyên.
+# 	YC2: Hàm trả về tổng các số lẻ trong danh sách đó.
+
+# Bài 8: Viết một hàm count_words(s) để đếm số lượng từ trong một chuỗi s.
+# 	YC1: Hàm nhận vào một chuỗi ký tự s.
+# 	YC2: Hàm trả về số lượng từ trong chuỗi đó.
