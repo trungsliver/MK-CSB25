3b357825f734e22c6e64f55896efdc54be4be76c
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-09-13 08:15:16 +0700

    Lesson 8

---
+++ Lesson8/practice1.py
@@ -0,0 +1,42 @@
+# ================ ÔN TẬP ================
+# Bài 1: Cho hai mảng số nguyên nums1 và nums2.
+# Viết chương trình trả về mảng là phép giao tập hợp của hai mảng trên.
+# Đánh giá độ phức tạp của thuật toán
+
+    # Khởi tạo 2 danh sách
+nums1 = [i for i in range(5,16)]    # 5 đến 15
+nums2 = [i for i in range(10,21)]   # 10 dến 20
+
+# Bài 2: Cho mảng nums gồm n quả bóng có màu đỏ, trắng, xanh.
+    # Viết chương trình sắp xếp mảng này sao cho:
+    # những quả bóng có màu giống nhau thì được sắp xếp cạnh nhau
+    # có thứ tự từ đỏ đến trắng đến xanh
+    # Đánh giá độ phức tạp của thuật toán.
+nums = ['red', 'white', 'blue', 'red', 'white', 'blue', 'red', 'white', 'blue']
+
+# ================= LUYỆN TẬP =================
+# Thực hiện và xác định độ phức tạp thuật toán của các bài tập sau
+
+# Bài 1: Viết một hàm sum_odd(numbers) để tính tổng các số lẻ trong một danh sách numbers.
+# 	YC1: Hàm nhận vào một danh sách các số nguyên.
+# 	YC2: Hàm trả về tổng các số lẻ trong danh sách đó.
+
+# Bài 2: Viết một hàm is_prime(n) để kiểm tra xem một số nguyên dương n có phải là số nguyên tố hay không.
+# 	YC1: Hàm nhận vào một số nguyên dương n.
+# 	YC2: Hàm trả về True nếu n là số nguyên tố, ngược lại trả về False.
+
+# Bài 3: Viết một hàm count_words(s) để đếm số lượng từ trong một chuỗi s.
+# 	YC1: Hàm nhận vào một chuỗi ký tự s.
+# 	YC2: Hàm trả về số lượng từ trong chuỗi đó.
+
+# Bài 4: Viết một hàm sum_of_digits(n) để tính tổng các chữ số của một số nguyên dương n.
+# 	YC1: Hàm nhận vào một số nguyên dương n.
+# 	YC2: Hàm trả về tổng các chữ số của n.
+
+# Bài 5: Viết một hàm find_max(numbers) để tìm vị trí số lớn nhất trong một danh sách numbers.
+# 	YC1: Hàm nhận vào một danh sách các số nguyên.
+# 	YC2: Hàm trả về vị trí số lớn nhất trong danh sách đó.
+
+# Bài 6: Viết một hàm sum_to_n(n) để tính tổng các số từ 1 đến n.
+# 	YC1: Hàm nhận vào một số nguyên dương n.
+# 	Yc2: Hàm trả về tổng các số từ 1 đến n.
---
+++ Lesson8/practice2.py
@@ -0,0 +1,31 @@
+# ================= Ôn tập =================
+
+# Bài 1: Nhập từ bàn phím điểm trung bình của học sinh. Hãy xếp hạng học lực của học sinh đó, biết rằng:
+# Giỏi: [8,10], Khá: [6.5, 8), TB: [5, 6.5), Yếu: [0, 5)
+# score = float(input("Nhập điểm trung bình của học sinh: "))
+
+# Bài 2: Nhập từ bàn phím số giây cần chuyển đổi. In ra màn hình thời gian sau chuyển đổi, định dạng: giờ - phút - giây
+# VD: 3661s = 1h 1m 1s
+
+# Bài 3: Nhập 2 số nguyên a và b từ bàn phím
+# Yêu cầu 1: In ra tất cả các số trong khoảng [a, b] hoặc ngược lại
+# Yêu cầu 2: Tính tổng các số chẵn trong khoảng vừa in
+# Yêu cầu 3: Tính trung bình cộng các số nguyên trong khoảng trên
+
+# Bài 4: Viết hàm/chương trình con kiểm tra 1 số có phải là số nguyên tố hay không, biết rằng: số nguyên tố là số chỉ chia hết cho 1 và chính nó.
+
+# Bài 5: Nhập vào từ bàn phím 1 chuỗi ký tự bao gồm các số nguyên, các số này cách nhau 1 khoảng trắng (hoặc dấu ,).
+# Yêu cầu 1: Tách chuỗi và thêm vào danh sách có tên num_list
+# Yêu cầu 2: Chuyển đổi toàn bộ phần tử trong danh sách num_list thành kiểu dữ liệu int.
+# Yêu cầu 3: In ra màn hình số lượng số lẻ của num_list.
+# Yêu cầu 4: Sắp xếp các phần tử trong danh sách num_list theo thứ tự từ lớn đến nhỏ.
+
+# Bài 6: Dùng thư viên random để thêm ngẫu nhiên các số nguyên trong khoảng [20,50] vào 2 danh sách arr1 và arr2. 
+# Yêu cầu 1: Hãy viết hàm/chương trình con in ra phần tử chung của 2 danh sách vừa tạo.
+# Yêu cầu 2: In ra màn hình vị trí phần tử nhỏ nhất của danh sách arr1
+# Yêu cầu 3: In ra màn hình vị trí phần tử lớn nhất của danh sách arr2
+
+# Bài 7: Hãy nhập từ bàn phím họ tên của bạn.
+# Yêu cầu 1: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết hoa
+# Yêu cầu 2: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết thường
+# Yêu cầu 1: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết hoa các chữ cái đầu
