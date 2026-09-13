c7923cd3747ac2181f67a7c3a714980752c2ab5a
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-09-13 09:48:20 +0700

    Lesson 8

--- Lesson8/practice1.py
+++ Lesson8/practice1.py
@@ -7,6 +7,43 @@
 nums1 = [i for i in range(5,16)]    # 5 đến 15
 nums2 = [i for i in range(10,21)]   # 10 dến 20
 
+    # Cách 1: Độ phức tạp O(n^2) - lặp 2 vòng lặp
+arr1 = []
+for item1 in nums1:
+    for item2 in nums2:
+        if item1 == item2:
+            arr1.append(item1)
+print("Phép giao tập hợp của 2 mảng là:", arr1)
+
+    # Cách 2: 
+def binary_search(arr, target):
+    arr.sort()      # Sắp xếp mảng để áp dụng tìm kiếm nhị phân
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return False
+
+arr2 = []
+if len(nums1) < len(nums2):
+    # swap phần tử 2 danh sách => nums1 luôn là danh sách dài hơn
+    nums1, nums2 = nums2, nums1 
+for item in nums2:
+    if binary_search(nums1, item):
+        arr2.append(item)
+print("Phép giao tập hợp của 2 mảng là:", arr2)
+    # Đánh giá độ phức tạp:
+            # Gọi n là kích thức nums1, m là kích thước nums2
+            # Hàm binary_search có độ phức tạp O(log m)
+            # Vòng lặp chạy n lần
+            # Tổng số lần lặp là n * O(log m) => độ phức tạp O(n log m)
+    
 # Bài 2: Cho mảng nums gồm n quả bóng có màu đỏ, trắng, xanh.
     # Viết chương trình sắp xếp mảng này sao cho:
     # những quả bóng có màu giống nhau thì được sắp xếp cạnh nhau
@@ -14,29 +51,107 @@ nums2 = [i for i in range(10,21)]   # 10 dến 20
     # Đánh giá độ phức tạp của thuật toán.
 nums = ['red', 'white', 'blue', 'red', 'white', 'blue', 'red', 'white', 'blue']
 
+def sort_colors(arr): # Độ phức tạp O(n)
+    # Đếm số lượng mỗi màu
+    count_red, count_white, count_blue = 0, 0, 0
+    for item in arr:
+        if item == 'red': count_red += 1
+        if item == 'white': count_white += 1
+        if item == 'blue': count_blue += 1
+
+    # Tạo mảng mới theo đúng thứ tự
+    result = ['red']*count_red + ['white']*count_white + ['blue']*count_blue
+    return result
+
+print("Mảng sau khi sắp xếp:", sort_colors(nums))
+
 # ================= LUYỆN TẬP =================
 # Thực hiện và xác định độ phức tạp thuật toán của các bài tập sau
 
 # Bài 1: Viết một hàm sum_odd(numbers) để tính tổng các số lẻ trong một danh sách numbers.
 # 	YC1: Hàm nhận vào một danh sách các số nguyên.
 # 	YC2: Hàm trả về tổng các số lẻ trong danh sách đó.
+def sum_odd(numbers): # Độ phức tạp O(n)
+    total = 0
+    for num in numbers:
+        if num % 2 != 0:
+            total += num
+    return total
 
 # Bài 2: Viết một hàm is_prime(n) để kiểm tra xem một số nguyên dương n có phải là số nguyên tố hay không.
 # 	YC1: Hàm nhận vào một số nguyên dương n.
 # 	YC2: Hàm trả về True nếu n là số nguyên tố, ngược lại trả về False.
+def is_prime(n): # Độ phức tạp O(√n)
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def is_prime2(n): # Độ phức tạp O(n)
+    count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            count += 1
+    if count == 2: return True
+    else: return False
 
 # Bài 3: Viết một hàm count_words(s) để đếm số lượng từ trong một chuỗi s.
 # 	YC1: Hàm nhận vào một chuỗi ký tự s.
 # 	YC2: Hàm trả về số lượng từ trong chuỗi đó.
+def count_words(s): # Độ phức tạp O(n)
+    # split(): tách chuỗi sau mỗi khoảng trắng
+    words = s.strip().split()
+    return len(words)
 
 # Bài 4: Viết một hàm sum_of_digits(n) để tính tổng các chữ số của một số nguyên dương n.
 # 	YC1: Hàm nhận vào một số nguyên dương n.
 # 	YC2: Hàm trả về tổng các chữ số của n.
+def sum_of_digits(n): # Độ phức tạp O(d) với d là số chữ số của n
+    total = 0
+    while n > 0:
+        digit = n % 10
+        total += digit
+        n //= 10
+    return total
+
+def sum_of_digits2(n): # Độ phức tạp O(log(n)) 
+    total = 0
+    # len(str(n)): số chữ số của n
+    for i in range(len(str(n))):
+        total += int(str(n)[i])
+    return total
 
 # Bài 5: Viết một hàm find_max(numbers) để tìm vị trí số lớn nhất trong một danh sách numbers.
 # 	YC1: Hàm nhận vào một danh sách các số nguyên.
 # 	YC2: Hàm trả về vị trí số lớn nhất trong danh sách đó.
+def find_max(numbers): # Độ phức tạp O(n)
+    max_value = numbers[0]
+    max_index = 0
+    for i in range(1, len(numbers)):
+        if numbers[i] > max_value:
+            max_value = numbers[i]
+            max_index = i
+    return max_index
+
+def find_max2(numbers): # Độ phức tạp O(n)
+    max_value = max(numbers)
+    for i in range(len(numbers)):
+        if numbers[i] == max_value:
+            return i
 
 # Bài 6: Viết một hàm sum_to_n(n) để tính tổng các số từ 1 đến n.
 # 	YC1: Hàm nhận vào một số nguyên dương n.
 # 	Yc2: Hàm trả về tổng các số từ 1 đến n.
+
+    # Cách 1:
+def sum_to_n(n):    # Độ phức tạp: O(n)
+    total = 0
+    for i in range(1, n+1):
+        total += i
+    return total
+
+    # Cách 2: công thức gauss
+def sum_to_n2(n):   # Độ phức tạp: O(1)
+    return n * (n + 1) / 2
--- Lesson8/practice2.py
+++ Lesson8/practice2.py
@@ -3,29 +3,167 @@
 # Bài 1: Nhập từ bàn phím điểm trung bình của học sinh. Hãy xếp hạng học lực của học sinh đó, biết rằng:
 # Giỏi: [8,10], Khá: [6.5, 8), TB: [5, 6.5), Yếu: [0, 5)
 # score = float(input("Nhập điểm trung bình của học sinh: "))
+score = 7.5
+
+def rank_student(score:float):
+    if 8 <= score <= 10:
+        print("Học lực: Giỏi")
+    elif 6.5 <= score < 8:
+        print("Học lực: Khá")   
+    elif 5 <= score < 6.5:
+        print("Học lực: Trung Bình")
+    elif 0 <= score < 5:
+        print("Học lực: Yếu")
+    else:
+        print("Điểm không hợp lệ")
+
+def rank_student2(score:float):
+    if score < 0 or score > 10:
+        print("Điểm không hợp lệ")
+    else:
+        if score >= 8:
+            print("Học lực: Giỏi")
+        elif score >= 6.5:
+            print("Học lực: Khá")   
+        elif score >= 5:
+            print("Học lực: Trung Bình")
+        else:
+            print("Học lực: Yếu")
+
+rank_student(score)
+rank_student2(score)
 
 # Bài 2: Nhập từ bàn phím số giây cần chuyển đổi. In ra màn hình thời gian sau chuyển đổi, định dạng: giờ - phút - giây
 # VD: 3661s = 1h 1m 1s
+# time = int(input("Nhập số giây cần chuyển đổi: "))
+time = 3661
+
+def convert_time(total_seconds:int):
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    return f"{total_seconds}s = {hours}h {minutes}m {seconds}s"
+
+print('Kết quả:', convert_time(time))
 
 # Bài 3: Nhập 2 số nguyên a và b từ bàn phím
+a = 5
+b = 10
+
 # Yêu cầu 1: In ra tất cả các số trong khoảng [a, b] hoặc ngược lại
+def print_range(a:int, b:int):
+    if a < b:
+        for i in range(a, b+1):
+            print(i, end=' ')
+    else:
+        for i in range(a, b-1, -1):
+            print(i, end=' ')
+print_range(a, b)
+
 # Yêu cầu 2: Tính tổng các số chẵn trong khoảng vừa in
+def sum_even(a:int, b:int):
+    total = 0
+    if a < b:
+        for i in range(a, b+1):
+            if i % 2 == 0:
+                total += i
+    else:
+        for i in range(b, a+1):
+            if i % 2 == 0:
+                total += i
+    return total
+print("\nTổng các số chẵn trong khoảng:", sum_even(a, b))
+
 # Yêu cầu 3: Tính trung bình cộng các số nguyên trong khoảng trên
+def average_range(a:int, b:int):
+    if a < b:
+        count = b - a + 1
+        total = sum(range(a, b+1))
+    else:
+        count = a - b + 1
+        total = sum(range(b, a+1))
+    return round(total / count, 2)
+print("Trung bình cộng các số trong khoảng:", average_range(a, b))
 
 # Bài 4: Viết hàm/chương trình con kiểm tra 1 số có phải là số nguyên tố hay không, biết rằng: số nguyên tố là số chỉ chia hết cho 1 và chính nó.
+def is_prime(n:int):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+# In ra số nguyên tố trong khoảng [10,100].
+print("Các số nguyên tố trong khoảng [10,100]:")
+for num in range(10, 101):
+    if is_prime(num):
+        print(num, end=' ')
 
 # Bài 5: Nhập vào từ bàn phím 1 chuỗi ký tự bao gồm các số nguyên, các số này cách nhau 1 khoảng trắng (hoặc dấu ,).
+input_str = '34 67 23 89 12 45 90 11'
+
 # Yêu cầu 1: Tách chuỗi và thêm vào danh sách có tên num_list
+num_list = input_str.split()
+print("\nDanh sách num_list:", num_list)
+
 # Yêu cầu 2: Chuyển đổi toàn bộ phần tử trong danh sách num_list thành kiểu dữ liệu int.
+for i in range(len(num_list)):
+    num_list[i] = int(num_list[i])
+print("Danh sách num_list sau khi chuyển đổi:", num_list)
+
 # Yêu cầu 3: In ra màn hình số lượng số lẻ của num_list.
+odd_count = 0
+for num in num_list:
+    if num % 2 != 0:
+        odd_count += 1
+print("Số lượng số lẻ trong num_list:", odd_count)
+
 # Yêu cầu 4: Sắp xếp các phần tử trong danh sách num_list theo thứ tự từ lớn đến nhỏ.
+num_list.sort(reverse=True)
+print("Danh sách num_list sau khi sắp xếp từ lớn đến nhỏ:", num_list)
 
 # Bài 6: Dùng thư viên random để thêm ngẫu nhiên các số nguyên trong khoảng [20,50] vào 2 danh sách arr1 và arr2. 
-# Yêu cầu 1: Hãy viết hàm/chương trình con in ra phần tử chung của 2 danh sách vừa tạo.
+import random
+arr1 = [random.randint(20, 50) for _ in range(10)]
+arr2 = [random.randint(20, 50) for _ in range(10)]
+# Yếu cầu 1: Hãy viết hàm/chương trình con in ra phần tử chung của 2 danh sách vừa tạo.
+def print_common_elements(list1, list2):
+    common_elements = set(list1) & set(list2)
+    return list(common_elements)
+print("Phần tử chung của arr1 và arr2:", print_common_elements(arr1, arr2))
+
+def print_common_elements(list1, list2): # Độ phức tạp O(n^2) / O(n*m)
+    arr_common = []
+    for item in list1:
+        if item in list2 and item not in arr_common:
+            arr_common.append(item)
+    return arr_common
+print("Phần tử chung của arr1 và arr2:", print_common_elements(arr1, arr2))
+
 # Yêu cầu 2: In ra màn hình vị trí phần tử nhỏ nhất của danh sách arr1
+    # Cách 1:
+min_value = min(arr1)
+min_index = arr1.index(min_value)
+print("Vị trí phần tử nhỏ nhất của arr1:", min_index)
+    # cách 2:
+min_value2 = arr1[0]
+min_index2 = 0
+for i in range(1, len(arr1)):
+    if arr1[i] < min_value2:
+        min_value2 = arr1[i]
+        min_index2 = i
+print("Vị trí phần tử nhỏ nhất của arr1:", min_index2)
+
 # Yêu cầu 3: In ra màn hình vị trí phần tử lớn nhất của danh sách arr2
+max_value = max(arr2)
+max_index = arr2.index(max_value)
+print("Vị trí phần tử lớn nhất của arr2:", max_index)
 
 # Bài 7: Hãy nhập từ bàn phím họ tên của bạn.
+name = 'Bui Duc Trung'
 # Yêu cầu 1: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết hoa
+print("Tên viết hoa các chữ cái đầu:", name.upper())
 # Yêu cầu 2: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết thường
+print("Tên viết thường:", name.lower())
 # Yêu cầu 1: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết hoa các chữ cái đầu
+print("Tên viết hoa các chữ cái đầu:", name.title())
